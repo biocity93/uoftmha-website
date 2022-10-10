3ea20acf02a9fe38cf646337a8fde09d5cad12f1
biocity93 <90408597+biocity93@users.noreply.github.com>
2022-10-10 11:47:14 -0400

    added style file

--- uoftmha-website-app/src/App.tsx
+++ uoftmha-website-app/src/App.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import styles  from './style';
 
 const App = () => (
   <div className="bg-primary w-full overflow-hidden">
-    <h1>Hello, world</h1>
+    <div className={'${styles.paddingX}'}>
+
+    </div>
     </div>
   )
 
